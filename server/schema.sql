d5872281e328c6bf798ab31f415b5ff798a082a5
byeo630 <byeo630@gmail.com>
2016-03-25 21:09:48 -0700

    Create database with tables inside

--- server/schema.sql
+++ server/schema.sql
@@ -2,16 +2,36 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
 
 /* Create other tables and define schemas for them here! */
 
+CREATE TABLE rooms (
+  /* Describe your table here.*/
+  id INT AUTO_INCREMENT, 
+  name VARCHAR(15), 
+  PRIMARY KEY (id)
+);
+CREATE TABLE users (
+  /* Describe your table here.*/
+  id INT AUTO_INCREMENT, 
+  name VARCHAR(15), 
+  PRIMARY KEY (id)
+);
 
-
+CREATE TABLE messages (
+  /* Describe your table here.*/
+  id INT AUTO_INCREMENT, 
+  fk_user INT,
+  fk_room INT, 
+  PRIMARY KEY (id), 
+  FOREIGN KEY (fk_user) 
+    REFERENCES users (id), 
+  FOREIGN KEY (fk_room) 
+    REFERENCES rooms (id)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
 
+-- ALTER TABLE MESSAGES passing foreign keys from users and from rooms
